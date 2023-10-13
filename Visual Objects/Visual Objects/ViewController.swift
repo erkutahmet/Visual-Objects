904bf501635628a6f69b02cdb645b8f082db44d4
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-13 04:29:06 +0300

    created Visual Objects xcode project

---
+++ Visual Objects/Visual Objects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Visual Objects
+//
+//  Created by Ahmet Erkut on 13.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
