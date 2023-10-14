dd584aea856046d9411ab344af6daa3ab4bd0232
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-14 16:29:18 +0300

    Slider, Stepper, Activity Indicator, ScrollView

--- Visual Objects/Visual Objects/ViewController.swift
+++ Visual Objects/Visual Objects/ViewController.swift
@@ -14,9 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var mSwitch: UISwitch!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var stepperLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        sliderLabel.text = String(Int(slider.value * 100))
+        stepperLabel.text = String(Int(stepper.value))
+        activityIndicator.isHidden = true
     }
     
     @IBAction func showResultsClicked(_ sender: Any) {
@@ -24,6 +32,8 @@ class ViewController: UIViewController {
         let selectedIndex = segmentedControl.selectedSegmentIndex
         let selectedCategory = segmentedControl.titleForSegment(at: selectedIndex)
         print("Segmented Control Data: \(selectedCategory!)")
+        print("Slider Value: \(slider.value * 100)")
+        print("Stepper Value: \(stepper.value)")
     }
     
     @IBAction func doClicked(_ sender: Any) {
@@ -60,6 +70,25 @@ class ViewController: UIViewController {
         print("Selected Status: \(selectedCategory!)")
     }
     
+    @IBAction func sliderChange(_ sender: UISlider) {
+        let value = Int(sender.value * 100)
+        sliderLabel.text = String(value)
+    }
+    
+    @IBAction func stepperChange(_ sender: UIStepper) {
+        let value = Int(sender.value)
+        stepperLabel.text = String(value)
+    }
+    
+    @IBAction func activityIndicatorStart(_ sender: Any) {
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+    }
+    
+    @IBAction func activityIndicatorStop(_ sender: Any) {
+        activityIndicator.stopAnimating()
+        activityIndicator.isHidden = true
+    }
     
 }
 
